88205356720e50b8d0fd1c336ee74adbfddccb0b
Brittani Lovelace <blovelace84@gmail.com>
2025-08-14 18:03:19 -0400

    trying to open presidents.dat file

--- CH09/09_07/09_07-challenge1.c
+++ CH09/09_07/09_07-challenge1.c
@@ -20,7 +20,25 @@ int main()
 	}
 
 	/* read records from back to front */
-
+	fseek(fp, 0, SEEK_END); /* go to the end of the file */
+	while( ftell(fp) > 0 ) /* while we are not at the beginning */
+	{
+		/* move back one record */
+		fseek(fp, - (long)sizeof(struct person), SEEK_CUR);
+		/* read the record */
+		x = fread(&president, sizeof(struct person), 1, fp);
+		if( x != 1 )
+		{
+			printf("Error reading record\n");
+			fclose(fp);
+			return(2);
+		}
+		/* display the record */
+		printf("%-20s Inaugurated: %4d Age: %2d\n",
+			president.name,
+			president.inauguration,
+			president.age);
+	}
 	/* clean-up */
 	fclose(fp);
 	return(0);
